82dd72f4400f04c663d9c608a14516a9ee189a3a
Daniel <dani.schroth@gmx.de>
2025-06-28 16:46:20 +0200

    switch CA to ContractInfo not env file

--- app/blackjack/BlackjackContractInformation.ts
+++ app/blackjack/BlackjackContractInformation.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import BlackjackABI from "../../ContractInfo/blackjackABI.json" with { type: "json" };
 
-const BlackjackContractAdress = process.env.CONTRACT_ADDRESS || "";
+const BlackjackContractAdress = "0x007D6661c37Edbe1837c90cb2797e9e691951671";
 
 export { BlackjackContractAdress, BlackjackABI };
